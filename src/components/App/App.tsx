a90fe2af784036bebbf35824809d423b02b62a93
Andrii Tsokur <tsokur@gmail.com>
2024-05-29 11:05:50 +0300

    bugFix: Refactored to exclude extra calculations

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,7 +8,11 @@ import {
 	defineScreenParams,
 	fetchApiConfigThunk,
 	fetchGenresThunk,
+	fetchTrendingThunk,
+	fetchUpcomingThunk,
 	selectService,
+	selectTrendingAll,
+	selectUpcoming,
 } from '@/redux';
 import { Loader } from '@/components';
 
@@ -20,6 +24,8 @@ export const App: React.FC = () => {
 		genres: { data: genres },
 		screen: { screenWidth },
 	} = useSelector(selectService);
+	const { day, week } = useSelector(selectTrendingAll);
+	const { data: upcoming } = useSelector(selectUpcoming);
 
 	// Handling screen resize
 	useEffect(() => {
@@ -36,14 +42,25 @@ export const App: React.FC = () => {
 
 	// Initial fetch of Service Details data
 	useEffect(() => {
-		if (apiConfig !== null) return;
-		dispatch<any>(fetchApiConfigThunk());
+		if (!apiConfig) dispatch<any>(fetchApiConfigThunk());
 	}, []);
 
 	useEffect(() => {
-		if (genres !== null) return;
-		dispatch<any>(fetchGenresThunk());
+		if (!genres) dispatch<any>(fetchGenresThunk());
 	}, []);
+
+	// Initial fetch of Movies Details data
+	useEffect(() => {
+		if (!day) dispatch<any>(fetchTrendingThunk('day'));
+	}, [dispatch, day]);
+
+	useEffect(() => {
+		if (!week) dispatch<any>(fetchTrendingThunk('week'));
+	}, [dispatch, week]);
+
+	useEffect(() => {
+		if (!upcoming) dispatch<any>(fetchUpcomingThunk());
+	}, [dispatch, upcoming]);
 
 	return <RouterProvider router={router} fallbackElement={<Loader />} />;
 };
