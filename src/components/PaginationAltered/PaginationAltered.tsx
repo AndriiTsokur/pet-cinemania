8bf5038ee5a05a7f4f700d54413d43571ed7bea0
Andrii Tsokur <tsokur@gmail.com>
2024-06-07 13:51:51 +0300

    feat: Adds styles to PaginationAltered

--- src/components/PaginationAltered/PaginationAltered.tsx
+++ src/components/PaginationAltered/PaginationAltered.tsx
@@ -1,7 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { Pagination } from '@mui/material';
 import styles from './PaginationAltered.module.css';
 import { MoviesDataT } from '@/utils';
+import { selectLocal, selectService } from '@/redux';
 
 type PropsT = {
 	data: MoviesDataT | null;
@@ -10,7 +12,30 @@ type PropsT = {
 
 export const PaginationAltered: React.FC<PropsT> = ({ data, pageName }) => {
 	const navigate = useNavigate();
+	const {
+		screen: { deviceType },
+	} = useSelector(selectService);
+	const { isDarkMode } = useSelector(selectLocal);
 	const { page: pageAddress } = useParams<{ page: string }>();
+
+	let paginationSize: any;
+	switch (deviceType) {
+		case 'tablet': {
+			paginationSize = 'medium';
+			break;
+		}
+		case 'desktop': {
+			paginationSize = 'large';
+			break;
+		}
+		default: {
+			paginationSize = 'small';
+			break;
+		}
+	}
+
+	const colorPrimary = isDarkMode ? '#ffffff' : '#111111';
+	const colorGrey = isDarkMode ? '#b7b7b7' : '#595959';
 
 	const handlePagination = (_: any, page: number) => {
 		navigate(page === 1 ? `/${pageName}` : `/${pageName}/${page}`);
@@ -25,6 +50,19 @@ export const PaginationAltered: React.FC<PropsT> = ({ data, pageName }) => {
 				disabled={!data || data.total_pages <= 1}
 				onChange={handlePagination}
 				page={pageAddress ? Number(pageAddress) : 1}
+				size={paginationSize}
+				sx={{
+					'& .MuiPaginationItem-root': {
+						borderColor: colorGrey,
+						color: colorPrimary,
+					},
+					'& .Mui-selected': {
+						borderColor: colorGrey,
+						background:
+							'linear-gradient(144.81deg, rgb(255, 194, 38) 9.233%,rgb(248, 65, 25) 92.699%)',
+						color: colorPrimary,
+					},
+				}}
 			/>
 		</div>
 	);
